4b022a6b8db2347ad274f30e3d88c8f7a7dee090
Magesh <magesh@hash14.com>
2018-11-15 19:23:50 +0530

    Todo form design is done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = function () {
-  return <h1>Hey guys</h1>
-}
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  );
+};
+
+const TodoForm = function () {
+  return (
+    <div>
+      <form className="form-group">
+        <input
+          type="text"
+          placeholder="Add a todo item"
+          className="form-control"></input>
+      </form>
+      <button className="btn btn-primary">Add</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
